6e49cfd0bdd2a6cde6d3587d02ed00092cb95301
Claire Vannier <clairevannierpro@gmail.com>
2024-03-04 13:15:57 +0100

    finish quests routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { BlockComponent } from "./block/block.component";
 import { UserProfileComponent } from "./user-profile/user-profile.component";
 import { AdminComponent } from "./admin/admin.component";
+import { MenuComponent } from "./menu/menu.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, BlockComponent, UserProfileComponent, AdminComponent]
+    imports: [RouterOutlet, BlockComponent, UserProfileComponent, AdminComponent, MenuComponent, RouterLink]
 })
 export class AppComponent {
   title = 'Angular-Quests';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: UserProfileComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'user', component: UserComponent}
+];
