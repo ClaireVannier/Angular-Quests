0efb578ce69d3d90893ea764a9b67e1a17d77464
Claire Vannier <clairevannierpro@gmail.com>
2024-03-06 13:14:53 +0100

    finish quests input

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SignupComponent } from './signup/signup.component';
 import { BlockComponent } from './block/block.component';
 import { AdminComponent } from './admin/admin.component';
 import { SearchMovieComponent } from './search-movies/search-movies.component';
+import { DeveloperComponent } from './developer/developer.component';
 
 export const routes: Routes = [
   { path: '', component: UserProfileComponent },
@@ -12,6 +13,8 @@ export const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'block', component: BlockComponent },
   { path: 'admin', component: AdminComponent },
-  { path: 'searchmovies', component: SearchMovieComponent}
+  { path: 'searchmovies', component: SearchMovieComponent},
+  { path: 'input', component: DeveloperComponent}
+
 
 ];
